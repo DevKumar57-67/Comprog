b8b10ea57f93ec607442054f43bcd992ddcc5011
Dev <devanshshukla957@gmail.com>
2026-08-15 17:00:46 +0530

    hello in cpp

---
+++ Addprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    a = 56;
+    b = 789;
+    int add;
+    add = a+b;
+    cout << "The sum is: " << add << endl;
+    return 0;
+    
+}
