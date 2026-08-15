b96dcc329d87110bfe2a0c0c7ec1269bf7edcfc1
Dev <devanshshukla957@gmail.com>
2026-08-16 00:40:32 +0530

    commit

--- Hello.cpp
+++ Hello.cpp
@@ -1,3 +1,7 @@
+
+// Cpp syntax
+
+
 #include <iostream>
 using namespace std;
 int main() {
@@ -5,3 +9,4 @@ int main() {
     return 0;
     
 }
+ 
---
+++ comments.cpp
@@ -0,0 +1,9 @@
+//comments in cpp
+// Ths is a single line comment
+/*
+This is a multiline comment
+
+comments are those statements which are ignored by the compiler and are only used to make
+the code more clear and easy to understand for non programmimg background people
+
+*/
