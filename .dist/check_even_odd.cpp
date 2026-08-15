a5945a1e6f48985acd562fd63fe789d2e0c33f06
Dev <devanshshukla957@gmail.com>
2026-08-16 01:11:42 +0530

    commit

---
+++ .dist/check_even_odd.cpp
@@ -0,0 +1,20 @@
+// This is a GeeksforGeeks C++ logic building basic problem 
+// C++ program to check if a number is even or odd
+
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num %2 == 0)
+    {
+        cout << "The number is even"<< endl;
+    }
+    else {
+        cout << "The number is odd" << endl;
+    }
+    return 0;
+    
+}
