fa98d5a69654c9bb28c395ea35d78708534f4411
Dev <devanshshukla957@gmail.com>
2026-08-16 13:14:43 +0530

    check_even_odd

---
+++ linear_search.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int key;
+    key = 5;
+    for(int i = 0; i<n; i++){
+        if(arr[i]== key){
+            cout << "Element found at index: " << i << endl;
+        }
+    }
+    return 0;
+
+}
